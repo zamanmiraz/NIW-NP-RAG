9909b8d14ae8ddb2c4cf499cf90dbb27b1c46db2
Miraz Zaman <mirazuzzaman.eee@gmail.com>
2025-11-06 00:50:30 -0500

    Test embedding model

--- test/test_rag.py
+++ test/test_rag.py
@@ -1,4 +1,6 @@
 from niw_np_rag.app.llm_rag import LLMRAG
+from niw_np_rag.app.rag import RAGPipeline
+import os
 
 def test_llm_rag_initialization():
     llm_rag = LLMRAG()
@@ -7,5 +9,18 @@ def test_llm_rag_initialization():
     print("LLMRAG initialized and response generated successfully.")
     print("Response:", response)
 
+def test_rag():
+    rag = RAGPipeline(pdfs_path="../data/uscis_aao_pdfs", vector_store_path="./data/chunks_vector_store", semantic_chunking=True)
+    # check if the the pdfs_path is set correctly
+    # find the exact path of the pdfs
+    ppath = rag.pdfs_path
+    # convert to absolute path
+    abs_ppath = os.path.abspath(ppath)
+    print("RAGPipeline initialized successfully.")
+    print(abs_ppath)
+    rag.build_vector_store_Qdrant()
+    print("RAGPipeline vector store built successfully.")
+
 if __name__ == "__main__":
-    test_llm_rag_initialization()
+    # test_llm_rag_initialization()
+    test_rag()
